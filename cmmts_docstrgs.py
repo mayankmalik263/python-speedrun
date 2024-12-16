fb6effe1da0a5e9df0a93a1c12777032524d4f03
mayankmalik263 <mayankmalik263@gmail.com>
2024-12-17 01:43:54 +0530

    well just discussed the basic comments and all stuff +  the help function which is pretty cool tho.

---
+++ cmmts_docstrgs.py
@@ -0,0 +1,23 @@
+# this is a comment
+## ----------------- ##
+"""
+this
+is
+a multiline
+comment
+"""
+## ----------------- ##
+'''
+this
+is 
+also 
+another 
+method 
+for a 
+multiline
+comment
+'''
+## ----------------- ##
+help(print) # help function helps to tell what the function will do
+help(classmethod) # well help function only works for one time in the program you cannot call it all over again and again which is obv
+
